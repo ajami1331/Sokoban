5d9aeced8cc478ab9d881256968192354c110a7d
Araf Al Jami <clown1331@gmail.com>
2023-03-04 22:30:30 +0000

    Update

--- raylib_c/scripts/prepare-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-sudo apt install -y \
-    libxinerama-dev \
-    libxrandr-dev \
-    libxcursor-dev \
-    libxi-dev \
-    libgl1-mesa-dev
-
- sudo apt-get install -y gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
