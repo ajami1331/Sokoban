1b393e164495340b7ba33ad762a1f13b8d476dce
Araf Al Jami <clown1331@gmail.com>
2023-03-10 02:18:28 +0000

    Add all gls

--- raylib_c/scripts/build-i686-w64-mingw32-windows.sh
+++ raylib_c/scripts/build-i686-w64-mingw32-windows.sh
@@ -1,29 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 echo $1
 cd $1
-cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_33 \
+
+declare -a opengl_versions=("43" "33" "21" "11")
+
+copied_once=false
+app_prefix="sokoban-gl"
+
+for gl_ver in "${opengl_versions[@]}"
+do
+    echo "Building OpenGL $gl_ver"
+    cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_$gl_ver \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
-cmake --build build-i686-windows
 
-mkdir -p temp
+    cmake --build build-i686-windows
 
-mkdir -p releases
-
-cp -r build-i686-windows/bin temp/bin-i686-windows
-cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl33.exe
-
-cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_21 \
-    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
-cmake --build build-i686-windows
-
-cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl21.exe
-
-cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_11 \
-    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
-cmake --build build-i686-windows
-
-cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl11.exe
+    if [ "$copied_once" = false ] ; then
+        mkdir -p temp
+        mkdir -p releases
+        cp -r build-i686-windows/bin temp/bin-i686-windows
+        copied_once=true
+    fi
+    cp build-i686-windows/bin/*.exe temp/bin-i686-windows/$app_prefix$gl_ver.exe
+done
 
 cd temp
 
