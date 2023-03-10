c38042be8f37f1a56efaf8c63063dcc737c1fb49
Araf Al Jami <clown1331@gmail.com>
2023-03-10 00:20:27 +0000

    Add opengl build for 33 21 11

--- raylib_c/scripts/build-i686-w64-mingw32-windows.sh
+++ raylib_c/scripts/build-i686-w64-mingw32-windows.sh
@@ -2,7 +2,7 @@
 set -e
 echo $1
 cd $1
-cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop \
+cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_33 \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
 cmake --build build-i686-windows
 
@@ -11,6 +11,19 @@ mkdir -p temp
 mkdir -p releases
 
 cp -r build-i686-windows/bin temp/bin-i686-windows
+cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl33.exe
+
+cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_21 \
+    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
+cmake --build build-i686-windows
+
+cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl21.exe
+
+cmake -S . -B build-i686-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_11 \
+    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-i686-w64-mingw32-windows.cmake
+cmake --build build-i686-windows
+
+cp build-i686-windows/bin/*.exe temp/bin-i686-windows/sokoban-gl11.exe
 
 cd temp
 
--- raylib_c/scripts/build-x86_64-w64-mingw32-windows.sh
+++ raylib_c/scripts/build-x86_64-w64-mingw32-windows.sh
@@ -2,8 +2,9 @@
 set -e
 echo $1
 cd $1
-cmake -S . -B build-x86_64-windows -G Ninja -DPLATFORM=Desktop \
+cmake -S . -B build-x86_64-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_33 \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-x86_64-w64-mingw32-windows.cmake
+
 cmake --build build-x86_64-windows
 
 mkdir -p temp
@@ -11,6 +12,22 @@ mkdir -p temp
 mkdir -p releases
 
 cp -r build-x86_64-windows/bin temp/bin-x86_64-windows
+
+cp build-x86_64-windows/bin/*.exe temp/bin-x86_64-windows/sokoban-gl33.exe
+
+cmake -S . -B build-x86_64-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_21 \
+    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-x86_64-w64-mingw32-windows.cmake
+
+cmake --build build-x86_64-windows
+
+cp build-x86_64-windows/bin/*.exe temp/bin-x86_64-windows/sokoban-gl21.exe
+
+cmake -S . -B build-x86_64-windows -G Ninja -DPLATFORM=Desktop -DGRAPHICS=GRAPHICS_API_OPENGL_11 \
+    -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-x86_64-w64-mingw32-windows.cmake
+
+cmake --build build-x86_64-windows
+
+cp build-x86_64-windows/bin/*.exe temp/bin-x86_64-windows/sokoban-gl11.exe
 
 cd temp
 
