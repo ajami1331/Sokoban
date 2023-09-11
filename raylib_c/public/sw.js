89b44035415e8924661bbdb8811d5fedaba56bdb
Araf Al Jami <clown1331@gmail.com>
2023-09-11 02:10:19 +0100

    Fix sw.js

--- raylib_c/public/sw.js
+++ raylib_c/public/sw.js
@@ -20,12 +20,7 @@ self.addEventListener('install', function (e) {
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
-            return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
+            return r || fetch(e.request);
         })
     );
 });
